5a623ed7c67d089dccd082d6a10162f1096a5fbb
jcpboho <71401807+jcpboho@users.noreply.github.com>
2020-11-13 15:34:21 -0500

    Update registroAlumnos.php

--- registroAlumnos.php
+++ registroAlumnos.php
@@ -10,29 +10,28 @@ header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
 
 
 
-  $resultados = file_get_contents('php://input');
+ // $resultados = file_get_contents('php://input');
 
  // $todo = json_decode($resultados, true);
-
-  $todo = json_decode($_REQUEST["param"], false, 512, JSON_UNESCAPED_UNICODE);
+//print_r($_REQUEST["param"]);
+  //$todo = json_decode($_REQUEST["param"]);
 
   //$ficha = preg_split("#\_([A-Z].*?)(.*?)\_#", $todo["periodo"]["id_externo"], null ,PREG_SPLIT_DELIM_CAPTURE);
   //$ficha = $ficha[2];
   $ficha = $_POST['ficha'];
   
-  $array = $todo["alumnos"];
+  //$array = $todo["alumnos"];
   //print_r($_POST);
   $con = new Conectar();
   $connection = $con->conexion();
   $connection->set_charset("utf8");
   $queryValues="";
   //foreach ($array as $value) {
-  while (list($clave, $value) = each($array)) {
+  while (list($clave, $value) = each($_REQUEST["param"])) {
      $id = $clave;
      $matricula = $value["matricula"];
      $nombre = $value["nombre"];
      $correo = $value["email"];
-     
      $queryValues .="('".$id."', '".$matricula."', '".$nombre."', '".$ficha."', '".$correo."'),";
 
   }
